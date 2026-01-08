cfd4f9c9022b5f4bee5a9c909c0a0184490db930
teamconspicuoussolutions-alt <teamconspicuoussolutions@gmail.com>
2026-01-08 10:27:36 +0530

    Update index.js

--- routes/web/index.js
+++ routes/web/index.js
@@ -3,7 +3,8 @@ const express = require('express');
 const router = express.Router();
 const profileRoutes = require('./profile'); 
 
-router.use('/profile', profileRoutes); 
+router.use('/', profileRoutes);
 
 
 module.exports = router;
+
