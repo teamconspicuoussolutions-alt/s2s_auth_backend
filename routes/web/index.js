06b5c38b3c8d1d4a9aa0192f2a26e1511efae5cf
teamconspicuoussolutions-alt <teamconspicuoussolutions@gmail.com>
2026-01-08 10:15:54 +0530

    Update index.js

--- routes/web/index.js
+++ routes/web/index.js
@@ -1,7 +1,9 @@
+// s2s_auth_backend/routes/web/index.js
 const express = require('express');
 const router = express.Router();
 const profileRoutes = require('./profile'); 
 
 router.use('/profile', profileRoutes); 
+
 
 module.exports = router;
