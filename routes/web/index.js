a3b82e28fffd5bda0a0b90fd5ce8ecc7da4a48eb
teamconspicuoussolutions-alt <teamconspicuoussolutions@gmail.com>
2026-01-09 12:18:32 +0530

    Update index.js

--- routes/web/index.js
+++ routes/web/index.js
@@ -3,10 +3,15 @@ const express = require('express');
 const router = express.Router();
 const profileRoutes = require('./profile'); 
 const subscriptionRoutes = require('./subscription');
+const addressRoutes = require('./address');
+const memberRoutes = require('./member');
 
 router.use('/', profileRoutes);
 router.use('/subscription', subscriptionRoutes);
+router.use('/addresses', addressRoutes);
+router.use('/members', memberRoutes);
 
 module.exports = router;
+
 
 
