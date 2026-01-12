b29def6777d8c107cc545e27c4dcc6b783bc7df6
teamconspicuoussolutions-alt <teamconspicuoussolutions@gmail.com>
2026-01-12 18:06:32 +0530

    Update wishlist.js

--- routes/web/wishlist.js
+++ routes/web/wishlist.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const prisma = require("../../prismaClient");
-const verifyToken = require("../../middleware/auth");
+const verifyToken = require("../../middleware/authMiddleware");
 
 
 
